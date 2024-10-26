66233373dbc449159af8c60b030737b04530b35a
Olawale Onabola <walexi4great@gmail.com>
2024-10-26 07:21:23 -0700

    pow, mean, log, exp

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -12,6 +12,9 @@ namespace cyg
     template <typename T>
      void op_cpu(std::vector<float>& out, const std::vector<float>& lhs, const std::vector<float>& rhs, T t);
 
+    std::vector<float> exp(const std::vector<float>& v1);
+    std::vector<float> mean(const std::vector<float>& v1, std::vector<int>& dims,  int dim);
+    std::vector<float> log(const std::vector<float>& v1);
     std::vector<float> pow(const std::vector<float>& v1, const float v2);
     std::vector<float> pow(const std::vector<float>& v1, const std::vector<float> v2);
     std::vector<float> operator+(const std::vector<float>& v1, const std::vector<float>& v2);
--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -1,4 +1,7 @@
 #include "arithmetic.h"
+#include "cmath"
+#include <algorithm>
+#include <numeric>
 using namespace std;
 using namespace cyg;
 /**
@@ -20,10 +23,9 @@ const auto op_mul = [](float a, float b)
 const auto op_pow = [](float a, float b)
 { return std::pow(a, b); };
 
-
 /**
  * @brief operation to run on cpu, plan to use openmpi for some speed ups
- * operation is between two vectors
+ * operation is executed on vectors
  * @TODO - consider extending to other types such as int, uint using generics
  * 
  * @param &out(type std::vector<float>) - output parameter
@@ -43,60 +45,101 @@ void cyg::op_cpu(std::vector<float> &out, const std::vector<float> &lhs, const s
  * operator overloading for vector, can easily delegate the underlying operations to cuda for speed up if cuda is available
  * 
  */
+std::vector<float> cyg::exp(const std::vector<float>& v1)
+{
+    vector<float> out_data;
+    out_data.reserve(v1.size());
+    for(const auto&j: v1) out_data.push_back(std::exp(v1[j]));
 
+    return out_data;
+}
+std::vector<float> cyg::mean(const std::vector<float>& v1, std::vector<int>& tdims, int dim)
+{
+    tdims.push_back(1);
+    auto shift = std::accumulate(next(tdims.cbegin(), dim+1), tdims.cend(), 1, multiplies<int>());
+    tdims.pop_back();
+    auto nums = tdims[dim];    
+    // @todo improve to o(n)
+    vector<float> out_data;
+    int start = 0;
+    while(start<v1.size()){
+        int m = start;
+        for(int k=0;k<shift;k++){
+            int c = 0;
+            float s = 0.0;
+            do{
+                s+=v1[m+shift * c];
+                c++;
+            }
+            while(c<nums);
+            out_data.push_back(s/nums);
+            m+=1;
+        }
+        start+=(shift*nums);
+    }
+    return out_data;
+}
+std::vector<float> cyg::log(const std::vector<float> &v1)
+{
+    vector<float> out_data;
+    out_data.reserve(v1.size());
+    for(const auto& j:v1) out_data.push_back(std::log(j));
+
+    return out_data;
+}
 std::vector<float> cyg::pow(const std::vector<float> &v1, const std::vector<float> v2)
 {
-    auto out_data = new vector<float>(v1.size(), 0);
-    op_cpu(*out_data, v1, v2, op_pow);
-    return *out_data;
+    vector<float> out_data(v1.size(),0);
+    op_cpu(out_data, v1, v2, op_pow);
+    return out_data;
 }
 std::vector<float> cyg::pow(const std::vector<float> &v1, const float v2)
 {
-    auto v2_vec = new vector<float>(v1.size(), v2);
-    return cyg::pow(v1, *v2_vec);
+    vector<float> v2_vec(v1.size(),0);
+    return cyg::pow(v1, v2_vec);
 }
 
 std::vector<float> cyg::operator+(const std::vector<float>& v1, const std::vector<float>& v2)
 {
-    auto out_data = new vector<float>(v1.size(), 0);
-    op_cpu(*out_data, v1, v2, op_add);
-    return *out_data;
+    vector<float> out_data(v1.size(),0);
+    op_cpu(out_data, v1, v2, op_add);
+    return out_data;
 }
 std::vector<float> cyg::operator+(const std::vector<float> &v1, const float v2)
 {
-    auto v2_vec = new vector<float>(v1.size(), v2);
-    return v1 + *v2_vec;
+    vector<float> v2_vec(v1.size(),0);
+    return v1 + v2_vec;
 }
 std::vector<float> cyg::operator-(const std::vector<float> &v1, const std::vector<float> &v2)
 {
-    auto out_data = new vector<float>(v1.size(), 0);
-    op_cpu(*out_data, v1, v2, op_sub);
-    return *out_data;
+    vector<float> out_data(v1.size(),0);
+    op_cpu(out_data, v1, v2, op_sub);
+    return out_data;
 }
 std::vector<float> cyg::operator-(const std::vector<float> &v1, const float v2)
 {
-    auto v2_vec = new vector<float>(v1.size(), v2);
-    return v1 - *v2_vec;
+    vector<float> v2_vec(v1.size(),0);
+    return v1 - v2_vec;
 }
 std::vector<float> cyg::operator*(const std::vector<float> &v1, const std::vector<float> &v2)
 {
-    auto out_data = new vector<float>(v1.size(), 0);
-    op_cpu(*out_data, v1, v2, op_mul);
-    return *out_data;
+    vector<float> out_data(v1.size(),0);
+    op_cpu(out_data, v1, v2, op_mul);
+    return out_data;
 }
 std::vector<float> cyg::operator*(const std::vector<float> &v1, const float v2)
 {
-    auto v2_vec = new vector<float>(v1.size(), v2);
-    return v1 * *v2_vec;
+    vector<float> v2_vec(v1.size(),0);
+    return v1 * v2_vec;
 }
 std::vector<float> cyg::operator/(const std::vector<float> &v1, const std::vector<float> &v2)
 {
-    auto out_data = new vector<float>(v1.size(), 0);
-    op_cpu(*out_data, v1, v2, op_mul);
-    return *out_data;
+    vector<float> out_data(v1.size(),0);
+    op_cpu(out_data, v1, v2, op_div);
+    return out_data;
 }
 std::vector<float> cyg::operator/(const std::vector<float> &v1, const float v2)
 {
-    auto v2_vec = new vector<float>(v1.size(), v2);
-    return v1 / *v2_vec;
+    vector<float> v2_vec(v1.size(),0);
+    return v1 / v2_vec;
 }
