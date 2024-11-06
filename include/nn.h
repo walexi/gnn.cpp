234cb1ab5a2560a3aff87c1d02ad9b0d5e946789
Olawale Onabola <walexi4great@gmail.com>
2024-11-05 19:12:33 -0800

    updated

--- include/nn.h
+++ include/nn.h
@@ -40,6 +40,7 @@ namespace nn
             for(auto& param : this->params){
                 param.second->enable_grad(isTrain);
             }
+            this->isTraining = isTrain;
         };
         virtual void eval(){ this->train(false);};
         virtual std::shared_ptr<cyg::tensor<float>> operator()(const std::shared_ptr<cyg::tensor<float>> &input_tensor) { return this->forward(input_tensor); };//you prolly dont wanna pass in a tensor without grad enabled
@@ -81,6 +82,95 @@ namespace nn
             return output;
         };
     };
+
+
+    class Sequential
+    {
+    protected:
+        std::shared_ptr<std::map<string, std::shared_ptr<Module>>> modules;
+
+    // };
+    // class ReLU : public Module
+    // {
+    // public:
+    //     ReLU() : Module() {};
+    //     std::shared_ptr<cyg::tensor> forward(const std::shared_ptr<cyg::tensor> &input_tensor);
+    // };
+
+    // class Dropout : public Module
+    // {
+    //     int p;
+    //     std::shared_ptr<tensor> drop;
+
+    // public:
+    //     Dropout(int p = 0.2) : Module(), p(p) {};
+    //     std::shared_ptr<cyg::tensor> forward(const std::shared_ptr<cyg::tensor> &input_tensor) override;
+    // }
+
+    // public:
+    //     Sequential(std::vector<std::shared_ptr<Module>> input_modules);
+    //     template <class T>
+    //     std::shared_ptr<cyg::tensor> operator()(const T &input_tensor);
+    //     void add_module(string name, std::shared_ptr<Module> m);
+    //     Sequential(const Sequential &seq) : modules(seq.modules) {} // rule of three/five/zero
+    //     ~Sequential()
+    //     {
+    //         this->modules.reset();
+    //     }
+    // };
+
+    // template <class T>
+    // class ReLU : public Operation<T>
+    // {
+    // public:
+    //     ReLU() : Operation<T>() {}
+    //     std::shared_ptr<T> forward(const std::shared_ptr<T> &t);
+    //     void backward(std::valarray<float> *incoming_grad) override;
+    // };
+
+    // template <class T>
+    // class Dropout : public Operation<T>
+    // {
+    //     double p;
+    //     bool isTrain;
+    //     std::shared_ptr<T> drop;
+
+    // public:
+    //     Dropout(double p = 0.2, bool isTrain = true) : Operation<T>(), p(p), isTrain(isTrain) {}
+    //     std::shared_ptr<T> forward(const std::shared_ptr<T> &t);
+    //     void set_isTrain(bool isTrain) { this->isTrain = isTrain; };
+    //     void backward(std::valarray<float> *incoming_grad) override;
+    // };
+
+    //     template<class T>
+    //     class Softmax : public Operation<T>{
+    //         int dim;
+    //         std::shared_ptr<T> drop;
+    //         public:
+    //             Softmax(int d): Operation<T>(), dim(d){}
+    //             std::shared_ptr<T> forward(const std::shared_ptr<T>& t);
+    //             void backward(std::vector<float>* incoming_grad) override;
+    //     };
+
+    //     template<class T>
+    //     class BatchNorm : public Operation<T>{
+
+    //         public:
+    //             BatchNorm(int num_features, float eps=1e-05, int momentum=0.1, bool affine=True, bool track_running_stats=True, cyg::Device device=Device::cpu): Operation<T>(), p(p){}
+    //             std::shared_ptr<T> forward(const std::shared_ptr<T>& t);
+    //             void backward(std::vector<float>* incoming_grad) override;
+    //     };
+
+    //     template<class T>
+    //     class LayerNorm : public Operation<T>{
+    //         std::shared_ptr<Parameter> weight;
+    //         std::shared_ptr<Parameter> b;
+    //         public:
+    //             LayerNorm(std::vector<int>> normalized_shape, float eps=1e-05, bool elementwise_affine=True, bool bias=True,cyg::Device device=None,): Operation<T>(), p(p){}
+    //             std::shared_ptr<T> forward(const std::shared_ptr<T>& t);
+    //             void backward(std::vector<float>* incoming_grad) override;
+    //     };
+
 
 
 
